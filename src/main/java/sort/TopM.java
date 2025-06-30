4bb7f408b08e0ea14c68c1436104c3c69ecc03c0
jongheonleee <qwefghnm1212@gmail.com>
2025-06-30 16:37:01 +0900

    sorting(2.5/4)

---
+++ src/main/java/sort/TopM.java
@@ -0,0 +1,15 @@
+package sort;
+
+import fundamental_concept.my_implements.MyStackByList;
+
+// 우선순위 큐
+public class TopM {
+
+    public static void main(String[] args) {
+
+
+
+        // 우선순위 큐 + 스택 => 정렬 구현
+        MyStackByList<Transaction> st = new MyStackByList<>();
+    }
+}
---
+++ src/main/java/sort/original/MaxPQ.java
@@ -0,0 +1,61 @@
+package sort.original;
+
+public class MaxPQ<Key extends Comparable<Key>> {
+
+    private Key[] pq; // 완전 이진 트리, pq[0]은 버리는 공간
+    private int N;
+
+    public MaxPQ(int maxN) {
+        pq = (Key[]) new Comparable[maxN + 1];
+    }
+
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void insert(Key v) {
+        pq[++N] = v;
+        swim(N);
+    }
+
+    public Key delMax() {
+        Key max = pq[1];
+        exch(1, N--);
+        pq[N+1] = null;
+        sink(1);
+        return max;
+    }
+
+    private boolean less(int i, int j) {
+        return pq[i].compareTo(pq[j]) < 0;
+    }
+
+    private void exch(int i, int j) {
+        Key tmp = pq[i];
+        pq[i] = pq[j];
+        pq[j] = tmp;
+    }
+
+    private void swim(int k) {
+        while (k > 1 && less(k/2, k)) {
+            exch(k/2, k);
+            k = k/2;
+        }
+    }
+
+    private void sink(int k) {
+        while (2*k <= N) {
+            int j = 2*k;
+            if (j < N && less(j, j+1)) j++;
+            if (!less(k, j)) break;
+            exch(k, j);
+            k = j;
+        }
+    }
+
+
+}
